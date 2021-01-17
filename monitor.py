acaf37d54a433f4bd15f4069c5140015548689f1
qdtroemner <qdtroemner@outlook.com>
2021-01-17 15:45:36 -0500

    organize?

--- monitor.py
+++
@@ -1,9 +0,0 @@
-import steam
-import datetime
-from time import sleep
-
-while True:
-	price = steam.GetLowestPrice("Operation Broken Fang Case")
-	print(datetime.datetime.now())
-	print(f'The current Operation Broken Fang Case price is ${price}.')
-	sleep(60 * 10)
--- upkeep.py
+++
@@ -1,6 +0,0 @@
-import telegram
-import os
-
-uptime = os.popen('uptime').readline()
-telegram.SendMessage("Still running PiServer.")
-telegram.SendMessage(uptime)
